07d06b99643d38b26648d8af0363d94489859f5c
Mark Gowen <mark.gowen78@gmail.com>
2019-10-10 12:26:45 -0700

    update
    created folders/files
    created store
    created initialState/reducers
    removed unnecessary items
    imported dependencies

---
+++ src/reducer/index.js
@@ -0,0 +1,18 @@
+
+
+const initialState = [
+    {
+        breweries: [],
+        isFetching: false,
+        error: ''
+    }
+]
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer;
